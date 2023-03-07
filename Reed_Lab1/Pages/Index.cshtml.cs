d5038f0c751d02607c0193cd107930df1f8262d1
alexhensley07 <alexhensley15@gmail.com>
2023-03-07 18:12:50 -0500

    search function

--- Reed_Lab1/Pages/Index.cshtml.cs
+++ Reed_Lab1/Pages/Index.cshtml.cs
@@ -9,4 +9,47 @@ namespace Reed_Lab1.Pages
         {
         }
     }
+
+
+    /*
+       /* 
+     public void OnPostSearchedInstructor()
+        {
+            if (SearchedInstructor == null)
+            {
+                SearchMessage = "Invalid Input, Please Enter First and Last Name";
+            }
+            else
+            {
+                String[] instructorNames = SearchedInstructor.Split(" ");
+                if (instructorNames.Length != 2)
+                {
+                    SearchMessage = "Invalid Input, Please Enter First and Last Name";
+                }
+                else
+                {
+                    SqlDataReader SIReader = DBClass.ReadInstructorAndDisplayOH(@instructorNames[0], @instructorNames[1]);
+                    while (SIReader.Read())
+                    {
+                        SelectedOHList.Add(new SelectedInstructorOH
+                        {
+                            instructorFirstName = SIReader["instructorFirstName"].ToString(),
+                            instructorLastName = SIReader["instructorLastName"].ToString(),
+                            ohDate = SIReader["ohDate"].ToString(),
+                            ohTimes = SIReader["ohTimes"].ToString(),
+                            ohLocation = SIReader["ohLocation"].ToString(),
+                            ohWaitLocation = SIReader["ohWaitLocation"].ToString(),
+                        });
+
+                    }
+                    DBClass.SchoolDBConnection.Close();
+
+                }
+            }
+
+        }
+     */
+
+
+    
 }
